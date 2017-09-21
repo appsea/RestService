3b5e53deb03194d1bbaaa9e671c703f1468c759e
Rakesh Girase <girase.rakesh@gmail.com>
2017-09-21 21:59:59 +0100

    first commit

---
+++ src/main/java/App.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by rakesh on 21-Sep-2017.
+ */
+public class App {
+}
---
+++ src/main/java/com/exuberant/survey/App.java
@@ -0,0 +1,10 @@
+package com.exuberant.survey;
+/**
+ * Created by rakesh on 21-Sep-2017.
+ */
+public class App {
+
+    public static void main(String[] args) {
+
+    }
+}
