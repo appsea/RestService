d81ffe54449c41afabf8c21b52df7109b4d46c51
Rakesh Girase <girase.rakesh@gmail.com>
2017-10-29 17:20:26 +0000

    FindAll, Find By Id and Create Flight Services along with logging Features

--- src/main/java/com/exuberant/survey/controller/BookMyFlightController.java
+++ src/main/java/com/exuberant/survey/controller/BookMyFlightController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
-@Slf4j
 @RestController
 @RequestMapping("/bookmyflights")
 public class BookMyFlightController {
