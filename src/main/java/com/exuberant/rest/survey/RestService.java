b624a0a2cc7179070885d9178d4c9ca81bf9173b
rakeshgirase <girase.rakesh@gmail.com>
2018-07-20 21:36:59 +0100

    TET Added

---
+++ src/main/java/com/exuberant/rest/survey/RestService.java
@@ -0,0 +1,21 @@
+package com.exuberant.rest.survey;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+public class RestService {
+
+    public void getRequest(String url){
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json");
+        headers.add("Cache-Control", "no-cache");
+        headers.add("Accept", "*/*");
+        HttpEntity<String> entity = new HttpEntity<>("parameters", headers);
+        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+        System.err.println("response: " + response.getBody());
+    }
+}
