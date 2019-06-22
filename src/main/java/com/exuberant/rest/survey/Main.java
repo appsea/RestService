4c9ac483f1dcfc4a04349862b5320250dff42f69
appsea <girase.rakesh@gmail.com>
2019-06-22 11:08:09 +0100

    Changes To Base SAS Questions

--- src/main/java/com/exuberant/rest/survey/Main.java
+++ src/main/java/com/exuberant/rest/survey/Main.java
@@ -46,7 +46,7 @@ public class Main {
         bank.add(new QuestionBank("dvsa.txt", 773, 773, 2, 10011, true, true, false));
         // bank.add(new QuestionBank("CompTIA A+.txt", 500, 977, 1, 10001, true, true));
         bank.add(new QuestionBank("Categories Base SAS.txt", 200, 434, 13, 10001, true, true));
-        bank.add(new QuestionBank("Base SAS Question.txt",200, 435, 8, 10100, true, true));
+        bank.add(new QuestionBank("Base SAS Question.txt",200, 435, 10, 10108, true, true));
         bank.add(new QuestionBank("Advance-sas-questions.txt", 150, 262, 7, 10072, true, true));
         return bank;
     }
